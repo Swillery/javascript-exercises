e89e1d5955bd6b2bf1f5a6079fda76ea7acde6e7
Swillery <213098486+Swillery@users.noreply.github.com>
2025-08-13 17:10:37 -0400

    Completed: javascript lessons 12-16

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0 || isNaN(num)) {return "OOPS"
+    };
+    let first = 0;
+    let second = 1;
+    if (num === 0) {return first
+    };
+        for (let i = 2; i <= num; i++) {
+            let sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return second;
 };
 
 // Do not edit below this line
